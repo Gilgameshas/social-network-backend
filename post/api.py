b5a694081fc7bb697b1ef72b08d66177b29aee6c
Dominykas <sanda.dominykas@gmail.com>
2026-04-23 13:24:09 +0300

    posts from frontend

--- post/api.py
+++ post/api.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view, authentication_classes, permissi
 
 from .models import Post
 from .serializers import PostSerializer
+from .forms import PostForm
 
 @api_view(['GET'])
 def post_list(request):
@@ -11,3 +12,17 @@ def post_list(request):
     serializer = PostSerializer(posts, many=True)
 
     return JsonResponse(serializer.data, safe=False)
+
+@api_view(['POST'])
+def post_create(request):
+    form = PostForm(request.data)
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.created_by = request.user
+        post.save()
+
+        serializer = PostSerializer(post)
+
+        return JsonResponse(serializer.data, safe=False)
+    else:
+        return JsonResponse({'error': 'add something here later!...'})
